Added plots for the stresses and the order parameter. Removed the density profile normalization: that's now fixed in the code

utils/plot_profiles.py
@@ -67,80 +67,118 @@ nbin=180
 rval=28.2094791
 testmap=LinearSegmentedColormap('test',cdict,N=len(vList))
 testmap2=LinearSegmentedColormap('test',cdict,N=len(JList))
+nstep=10000000
+nsave=10000
+nsnap=int(nstep/nsave)
+skip=int(nsnap/2)
+dt=0.001
 
 
 # Profiles
 # Set column to plot
-usecolumn=6
+usecolumn=8
 
-profList=[r'$\theta$',r'$\rho$',r'$\sqrt{\langle v^2 \rangle}/v_0$','energy','pressure',r'$\alpha$',r'$\alpha_v$']
-profName=['theta','rho','vrms','energy','pressure','alpha','alpha_v']
+profList=[r'$\theta$',r'$\rho$',r'$\sqrt{\langle v^2 \rangle}/v_0$','energy','pressure',r'$\Sigma_{\theta \theta}$',r'$\Sigma_{\theta \phi}$',r'$\Sigma_{\phi \theta}$',r'$\Sigma_{\phi \phi}$',r'$\alpha$',r'$\alpha_v$']
+profName=['theta','rho','vrms','energy','pressure','stt','stp','spt','spp','alpha','alpha_v']
 
-for j in range(len(JList)):
-	plt.figure(figsize=(10,7),linewidth=2.0)
-	for i in range(len(vList)):
-		print vList[i],JList[j]
-		ax=plt.gca()
-		outfile=outfolder+'data/profilesV_v0' + vList[i] + '_j' + JList[j] + '.dat'
-		outfile2=outfolder + 'data/axisV_v0' + vList[i] + '_j' + JList[j] + '.dat'
-		# header='theta rho vel energy pressure alpha alpha_v'
-		profiles=sp.loadtxt(outfile, unpack=True)[:,:] 
-		isdata=[index for index,value in enumerate(profiles[1,:]) if (value >0)]
-		# Forgot the normalization of rho by the angle band width
-		if usecolumn==1:
-			normz=2*np.pi*rval*abs(np.cos(profiles[0,:]))
-			profiles[1,isdata]=profiles[1,isdata]/normz[isdata]
-			profiles[1,:]/=np.mean(profiles[1,:])
-		if usecolumn==2:
-			plt.plot(profiles[0,isdata],profiles[usecolumn,isdata]/float(vList[i]),color=testmap(i), linestyle='solid',label=vList[i])
-		else:
-			plt.plot(profiles[0,isdata],profiles[usecolumn,isdata],color=testmap(i), linestyle='solid',label=vList[i])
-	if usecolumn<=4:
-		plt.ylim(0,1.25*profiles[usecolumn,nbin/2])
-	if usecolumn==5:
-		plt.plot(profiles[0,isdata],profiles[0,isdata],'k-')
-		plt.ylim(-0.5,0.5)
-	plt.xlim(-np.pi/2,np.pi/2)
-	plt.xlabel(profList[0]) 
-	plt.ylabel(profList[usecolumn]) 
-	plt.legend(loc=2,ncol=2)
-	plt.title('Interaction strength ' + r'$J=$' + JList[j])
+#for j in range(len(JList)):
+	#plt.figure(figsize=(10,7),linewidth=2.0)
+	#for i in range(len(vList)):
+		#print vList[i],JList[j]
+		#ax=plt.gca()
+		#outfile=outfolder+'data/profilesV_v0' + vList[i] + '_j' + JList[j] + '.dat'
+		#outfile2=outfolder + 'data/axisV_v0' + vList[i] + '_j' + JList[j] + '.dat'
+		## header='theta rho vel energy pressure alpha alpha_v'
+		#profiles=sp.loadtxt(outfile, unpack=True)[:,:] 
+		#isdata=[index for index,value in enumerate(profiles[1,:]) if (value >0)]
+		## Corrected
+		### Forgot the normalization of rho by the angle band width
+		##if usecolumn==1:
+			##normz=2*np.pi*rval*abs(np.cos(profiles[0,:]))
+			##profiles[1,isdata]=profiles[1,isdata]/normz[isdata]
+			##profiles[1,:]/=np.mean(profiles[1,:])
+		#if usecolumn==2:
+			#plt.plot(profiles[0,isdata],profiles[usecolumn,isdata]/float(vList[i]),color=testmap(i), linestyle='solid',label=vList[i])
+		#else:
+			#plt.plot(profiles[0,isdata],profiles[usecolumn,isdata],color=testmap(i), linestyle='solid',label=vList[i])
+	##if usecolumn<=8:
+		##plt.ylim(0,1.25*profiles[usecolumn,nbin/2])
+	#if usecolumn==9:
+		#plt.plot(profiles[0,isdata],profiles[0,isdata],'k-')
+		#plt.ylim(-0.5,0.5)
+	#plt.xlim(-np.pi/2,np.pi/2)
+	#plt.xlabel(profList[0]) 
+	#plt.ylabel(profList[usecolumn]) 
+	#plt.legend(loc=2,ncol=2)
+	#plt.title('Interaction strength ' + r'$J=$' + JList[j])
 	
-	filename=outfolder + 'pics/profile_' + profName[usecolumn] + '_J' + JList[j] +'.pdf'
-	plt.savefig(filename)
+	#filename=outfolder + 'pics/profile_' + profName[usecolumn] + '_J' + JList[j] +'.pdf'
+	#plt.savefig(filename)
 
-for i in range(len(vList)):	
-	plt.figure(figsize=(10,7),linewidth=2.0)
-	for j in range(len(JList)):
-		print vList[i],JList[j]
-		ax=plt.gca()
-		outfile=outfolder+'data/profilesV_v0' + vList[i] + '_j' + JList[j] + '.dat'
-		outfile2=outfolder + 'data/axisV_v0' + vList[i] + '_j' + JList[j] + '.dat'
-		# header='theta rho vel energy pressure alpha alpha_v'
-		profiles=sp.loadtxt(outfile, unpack=True)[:,:] 
-		isdata=[index for index,value in enumerate(profiles[1,:]) if (value >0)]
-		if usecolumn==1:
-			normz=2*np.pi*rval*abs(np.cos(profiles[0,:]))
-			profiles[1,isdata]=profiles[1,isdata]/normz[isdata]
-			profiles[1,:]/=np.mean(profiles[1,:])
-		if usecolumn==2:
-			plt.plot(profiles[0,isdata],profiles[usecolumn,isdata]/float(vList[i]),color=testmap2(j), linestyle='solid',label=JList[j])
-		else:
-			plt.plot(profiles[0,isdata],profiles[usecolumn,isdata],color=testmap2(j), linestyle='solid',label=JList[j])
-	if usecolumn==5:
-		plt.plot(profiles[0,isdata],profiles[0,isdata],'k-')
-		plt.ylim(-0.5,0.5)
-	if usecolumn<=4:
-		plt.ylim(0,1.25*profiles[usecolumn,nbin/2])
-	plt.xlim(-np.pi/2,np.pi/2)
-	plt.xlabel(profList[0]) 
-	plt.ylabel(profList[usecolumn]) 
-	plt.legend(loc=2,ncol=2)
-	plt.title('Velocity ' + r'$v_0=$' + vList[i])
+#for i in range(len(vList)):	
+	#plt.figure(figsize=(10,7),linewidth=2.0)
+	#for j in range(len(JList)):
+		#print vList[i],JList[j]
+		#ax=plt.gca()
+		#outfile=outfolder+'data/profilesV_v0' + vList[i] + '_j' + JList[j] + '.dat'
+		#outfile2=outfolder + 'data/axisV_v0' + vList[i] + '_j' + JList[j] + '.dat'
+		## header='theta rho vel energy pressure alpha alpha_v'
+		#profiles=sp.loadtxt(outfile, unpack=True)[:,:] 
+		#isdata=[index for index,value in enumerate(profiles[1,:]) if (value >0)]
+		## Corrected
+		##if usecolumn==1:
+			##normz=2*np.pi*rval*abs(np.cos(profiles[0,:]))
+			##profiles[1,isdata]=profiles[1,isdata]/normz[isdata]
+			##profiles[1,:]/=np.mean(profiles[1,:])
+		#if usecolumn==2:
+			#plt.plot(profiles[0,isdata],profiles[usecolumn,isdata]/float(vList[i]),color=testmap2(j), linestyle='solid',label=JList[j])
+		#else:
+			#plt.plot(profiles[0,isdata],profiles[usecolumn,isdata],color=testmap2(j), linestyle='solid',label=JList[j])
+	#if usecolumn==9:
+		#plt.plot(profiles[0,isdata],profiles[0,isdata],'k-')
+		#plt.ylim(-0.5,0.5)
+	##if usecolumn<=8:
+		##plt.ylim(0,1.25*profiles[usecolumn,nbin/2])
+	#plt.xlim(-np.pi/2,np.pi/2)
+	#plt.xlabel(profList[0]) 
+	#plt.ylabel(profList[usecolumn]) 
+	#plt.legend(loc=2,ncol=2)
+	#plt.title('Velocity ' + r'$v_0=$' + vList[i])
 	
-	filename=outfolder + 'pics/profile_' + profName[usecolumn] + '_v' + vList[i] +'.pdf'
-	plt.savefig(filename)
+	#filename=outfolder + 'pics/profile_' + profName[usecolumn] + '_v' + vList[i] +'.pdf'
+	#plt.savefig(filename)
 	
+	
+#for i in range(len(vList)):	
+	#plt.figure(figsize=(10,7),linewidth=2.0)
+	#for j in range(len(JList)):
+		#print vList[i],JList[j]
+		#ax=plt.gca()
+		#outfile=outfolder+'data/profilesV_v0' + vList[i] + '_j' + JList[j] + '.dat'
+		#outfile2=outfolder + 'data/axisV_v0' + vList[i] + '_j' + JList[j] + '.dat'
+		## header='theta rho vel energy pressure alpha alpha_v'
+		#profiles=sp.loadtxt(outfile, unpack=True)[:,:] 
+		#isdata=[index for index,value in enumerate(profiles[1,:]) if (value >0)]
+		#if j==0:
+			#plt.plot(profiles[0,isdata],profiles[4,isdata],color='k', linestyle='solid',label=r'$Tr(\Sigma)$')
+			#plt.plot(profiles[0,isdata],profiles[5,isdata],color='r', linestyle='solid',label=r'$\Sigma_{\theta \theta}$')
+			#plt.plot(profiles[0,isdata],profiles[6,isdata],color='g', linestyle='solid',label=r'$\Sigma_{\theta \phi}$')
+			#plt.plot(profiles[0,isdata],profiles[8,isdata],color='b', linestyle='solid',label=r'$\Sigma_{\phi \phi}$')
+		#else:
+			#plt.plot(profiles[0,isdata],profiles[4,isdata],color='k', linestyle='solid')
+			#plt.plot(profiles[0,isdata],profiles[5,isdata],color='r', linestyle='solid')
+			#plt.plot(profiles[0,isdata],profiles[6,isdata],color='g', linestyle='solid')
+			#plt.plot(profiles[0,isdata],profiles[8,isdata],color='b', linestyle='solid')
+	##plt.xlim(-np.pi/2,np.pi/2)
+	#plt.xlabel(profList[0]) 
+	#plt.ylabel('stresses') 
+	#plt.legend(loc=2,ncol=2)
+	#plt.title('Velocity ' + r'$v_0=$' + vList[i])
+	
+	#filename=outfolder + 'pics/stress_profiles_v' + vList[i] +'.pdf'
+	#plt.savefig(filename)
+	
+## Axis in 3d
 #for i in range(len(vList)):	
 	#fig=plt.figure(figsize=(10,7),linewidth=2.0)
 	#ax = fig.add_subplot(111, projection='3d')
@@ -162,6 +200,53 @@ for i in range(len(vList)):
 	#plt.title('Velocity ' + r'$v_0=$' + vList[i])
 	
 	#filename=outfolder + 'pics/axisV_' + profName[usecolumn] + '_v' + vList[i] +'.pdf'
+	#plt.savefig(filename)
+	
+## Order parameter n = |\frac{1}{N R v_0} \sum r \times v|
+#for i in range(len(vList)):	
+	#fig=plt.figure(figsize=(10,7),linewidth=2.0)
+	#for j in range(len(JList)):
+		#print vList[i],JList[j]
+		#ax=plt.gca()
+		#outfile=outfolder+'data/profilesV_v0' + vList[i] + '_j' + JList[j] + '.dat'
+		#outfile2=outfolder + 'data/axisV_v0' + vList[i] + '_j' + JList[j] + '.dat'
+		#axis=sp.loadtxt(outfile2, unpack=True)[:,:] 
+		#orderpar=np.sqrt(axis[3,:]**2+axis[4,:]**2+axis[5,:]**2)
+		#xval=np.linspace(skip*nsave*dt,nstep*dt,(nsnap-skip+2))
+		#plt.plot(xval,orderpar,color=testmap2(j), linestyle='solid',label=JList[j])
+	#plt.xlabel('x') 
+	#plt.ylabel('y') 
+	#plt.ylim(0,1)
+	#plt.legend(loc=3,ncol=2)
+	#plt.title('Velocity ' + r'$v_0=$' + vList[i])
+	
+	#filename=outfolder + 'pics/orderpar_v' + vList[i] +'.pdf'
+	#plt.savefig(filename)
+	
+# Order parameter n = |\frac{1}{N R v_0} \sum r \times v|
+fig=plt.figure(figsize=(10,7),linewidth=2.0)
+ax=plt.gca()
+orderpar=np.zeros((len(vList),))
+dorder=np.zeros((len(vList),))
+vval=np.zeros((len(vList),))
+for j in range(len(JList)):
+	for i in range(len(vList)):	
+		print vList[i],JList[j]
+		outfile=outfolder+'data/profilesV_v0' + vList[i] + '_j' + JList[j] + '.dat'
+		outfile2=outfolder + 'data/axisV_v0' + vList[i] + '_j' + JList[j] + '.dat'
+		axis=sp.loadtxt(outfile2, unpack=True)[:,:] 
+		orderpar0=np.sqrt(axis[3,:]**2+axis[4,:]**2+axis[5,:]**2)
+		orderpar[i]=np.mean(orderpar0)
+		dorder[i]=np.std(orderpar0) 
+		vval[i]=np.log10(float(vList[i]))
+	plt.errorbar(vval,orderpar,yerr=dorder,color=testmap2(j), linestyle='solid',label='J='+JList[j])
+plt.xlabel(r'$\log_{10}v_0$') 
+plt.ylabel('n') 
+plt.ylim(0,1)
+plt.legend(loc=2,ncol=1)
+plt.title('Order parameter')
+	
+	#filename=outfolder + 'pics/orderpar_v' + vList[i] +'.pdf'
 	#plt.savefig(filename)
 	
 