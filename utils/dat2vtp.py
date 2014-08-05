7f42788b30e51c075bdbb8ba230a03e15c2f4e35
Rastko Sknepnek <sknepnek@gmail.com>
2014-08-05 12:24:36 +0100

    Made dat2vtp convertor much faster.

--- utils/dat2vtp.py
+++ utils/dat2vtp.py
@@ -23,12 +23,14 @@ import argparse
 from read_data import *
 import numpy as np
 from datetime import *
+from glob import glob
 import vtk
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-i", "--input", type=str, help="input file")
-parser.add_argument("-o", "--output", type=str, help="output file")
+parser.add_argument("-i", "--input", type=str, help="input file (base name)")
+parser.add_argument("-o", "--output", type=str, help="output directory")
+parser.add_argument("-s", "--skip", type=int, default=0, help="skip this many samples")
 args = parser.parse_args()
 
 print
@@ -42,83 +44,90 @@ print "\t----------------------------------------------"
 print
 print "\tInput files : ", args.input
 print "\tOutput files : ", args.output
+print "\tSkip frames : ", args.skip
 print
 
 start = datetime.now()
 
-Points = vtk.vtkPoints()
+files = sorted(glob(args.input+'*.dat'))[args.skip:]
+
+for f in files:
+  print "Processing file : ", f
+
+  Points = vtk.vtkPoints()
 
 
-has_v = False
-has_n = False
+  has_v = False
+  has_n = False
 
-data = ReadData(args.input)
+  data = ReadData(f)
 
-if not (data.keys.has_key('x') and data.keys.has_key('y') and data.keys.has_key('z')):
-  raise "Particle coordinate not specified in the input data."
+  if not (data.keys.has_key('x') and data.keys.has_key('y') and data.keys.has_key('z')):
+    raise "Particle coordinate not specified in the input data."
 
-x = np.array(data.data[data.keys['x']])
-y = np.array(data.data[data.keys['y']])
-z = np.array(data.data[data.keys['z']])
+  x = np.array(data.data[data.keys['x']])
+  y = np.array(data.data[data.keys['y']])
+  z = np.array(data.data[data.keys['z']])
 
 
-if (data.keys.has_key('vx') or data.keys.has_key('vy') or data.keys.has_key('vz')):
-  vx = np.array(data.data[data.keys['vx']])
-  vy = np.array(data.data[data.keys['vy']])
-  vz = np.array(data.data[data.keys['vz']])
-  has_v = True
+  if (data.keys.has_key('vx') or data.keys.has_key('vy') or data.keys.has_key('vz')):
+    vx = np.array(data.data[data.keys['vx']])
+    vy = np.array(data.data[data.keys['vy']])
+    vz = np.array(data.data[data.keys['vz']])
+    has_v = True
 
-if (data.keys.has_key('nx') or data.keys.has_key('ny') or data.keys.has_key('nz')):
-  nx = np.array(data.data[data.keys['nx']])
-  ny = np.array(data.data[data.keys['ny']])
-  nz = np.array(data.data[data.keys['nz']])
-  has_n = True
+  if (data.keys.has_key('nx') or data.keys.has_key('ny') or data.keys.has_key('nz')):
+    nx = np.array(data.data[data.keys['nx']])
+    ny = np.array(data.data[data.keys['ny']])
+    nz = np.array(data.data[data.keys['nz']])
+    has_n = True
 
-r = np.ones(len(x))  
+  r = np.ones(len(x))  
 
-Radii = vtk.vtkDoubleArray()
-Radii.SetNumberOfComponents(1)
-Radii.SetName('Radius')
+  Radii = vtk.vtkDoubleArray()
+  Radii.SetNumberOfComponents(1)
+  Radii.SetName('Radius')
 
-if has_v:
-  Velocities = vtk.vtkDoubleArray()
-  Velocities.SetNumberOfComponents(3)
-  Velocities.SetName("Velocity")
+  if has_v:
+    Velocities = vtk.vtkDoubleArray()
+    Velocities.SetNumberOfComponents(3)
+    Velocities.SetName("Velocity")
 
-if has_n:
-  Directors = vtk.vtkDoubleArray()
-  Directors.SetNumberOfComponents(3)
-  Directors.SetName("Directors")
+  if has_n:
+    Directors = vtk.vtkDoubleArray()
+    Directors.SetNumberOfComponents(3)
+    Directors.SetName("Directors")
 
-for (xx,yy,zz,rr) in zip(x,y,z,r):
-  Points.InsertNextPoint(xx,yy,zz)
-  Radii.InsertNextValue(rr)
-  
-if has_v:
-  for (vvx,vvy,vvz) in zip(vx,vy,vz):
-    Velocities.InsertNextTuple3(vvx,vvy,vvz)
+  for (xx,yy,zz,rr) in zip(x,y,z,r):
+    Points.InsertNextPoint(xx,yy,zz)
+    Radii.InsertNextValue(rr)
+    
+  if has_v:
+    for (vvx,vvy,vvz) in zip(vx,vy,vz):
+      Velocities.InsertNextTuple3(vvx,vvy,vvz)
 
-if has_n:
-  for (nnx,nny,nnz) in zip(nx,ny,nz):
-    Directors.InsertNextTuple3(nnx,nny,nnz)
+  if has_n:
+    for (nnx,nny,nnz) in zip(nx,ny,nz):
+      Directors.InsertNextTuple3(nnx,nny,nnz)
 
-polydata = vtk.vtkPolyData()
-polydata.SetPoints(Points)
+  polydata = vtk.vtkPolyData()
+  polydata.SetPoints(Points)
 
-polydata.GetPointData().AddArray(Radii)
+  polydata.GetPointData().AddArray(Radii)
 
-if has_v:
-  polydata.GetPointData().AddArray(Velocities)
+  if has_v:
+    polydata.GetPointData().AddArray(Velocities)
 
-if has_n:
-  polydata.GetPointData().AddArray(Directors)
+  if has_n:
+    polydata.GetPointData().AddArray(Directors)
 
-polydata.Modified()
-writer = vtk.vtkXMLPolyDataWriter()
-writer.SetFileName(args.output+'.vtp')
-writer.SetInputData(polydata)
-writer.SetDataModeToAscii()
-writer.Write()
+  polydata.Modified()
+  writer = vtk.vtkXMLPolyDataWriter()
+  outname = '.'.join(f.split('.')[:-1])
+  writer.SetFileName(args.output+'/'+outname+'.vtp')
+  writer.SetInputData(polydata)
+  writer.SetDataModeToAscii()
+  writer.Write()
 
 
 end = datetime.now()
